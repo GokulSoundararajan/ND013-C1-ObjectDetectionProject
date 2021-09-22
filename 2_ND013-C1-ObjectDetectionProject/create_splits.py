9b06e9e87a1fd00d133fe2801bc1569296f35bc9
Gokul <gokuls@live.in>
2021-09-22 18:54:14 +0530

    Update create_splits.py

--- 2_ND013-C1-ObjectDetectionProject/create_splits.py
+++ 2_ND013-C1-ObjectDetectionProject/create_splits.py
@@ -1,7 +1,7 @@
 import argparse
 import glob
 import os
-import random
+
 
 import numpy as np
 
@@ -17,7 +17,40 @@ def split(data_dir):
         - data_dir [str]: data directory, /mnt/data
     """
     # TODO: Implement function
+    trainDir = data_dir + '/train'
+    validationDir =  data_dir + '/val'
+    testDir = data_dir + '/test'
+
+    try:
+        # Create target Directories
+        os.mkdir(trainDir)
+        print(trainDir + " Created")
+        os.mkdir(validationDir)
+        print(validationDir + " Created")
+        os.mkdir(testDir)
+        print(testDir + " Created") 
+    except FileExistsError:
+        print("Directory already exists")
+
+    fileArray = glob.glob(data_dir)
+
+
     
+    #Split the .tfrecord files in different folders in the ratio 80:10:10. 
+    for name in fileArray:
+        if( (fileArray.index(name)+1)/len(fileArray)*100 <= 80 and os.path.isfile(name) ):
+            head_tail = os.path.split(name)
+            newpath = os.path.join(trainDir, head_tail[1])
+            os.rename(name, newpath)
+        elif( (fileArray.index(name)+1)/len(fileArray)*100 > 80 and (fileArray.index(name)+1)/len(fileArray)*100 <= 90 and os.path.isfile(name)):
+            head_tail = os.path.split(name)
+            newpath = os.path.join(validationDir,  head_tail[1])
+            os.rename(name, newpath)
+        else:
+            if(os.path.isfile(name)):
+                head_tail = os.path.split(name)
+                newpath = os.path.join(testDir, head_tail[1])
+                os.rename(name, newpath)
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
